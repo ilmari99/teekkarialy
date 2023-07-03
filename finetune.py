5c6059eecc09ee22d6aa0f4e3f6abfd9a1e3bd8e
ilmari99 <i.vahteristo@gmail.com>
2023-07-03 15:55:19 +0300

    Added 'overlap' that specifies, how many windows (seq_len consecutive messages) to skip. Recommended to be seq_len-1

--- finetune.py
+++ finetune.py
@@ -14,49 +14,60 @@ import time
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.cuda.amp import autocast
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 import random
 import numpy as np
 import os
-#from LanguageModel import LanguageModel
+#os.environ["PYTORCH_NO_CUDA_MEMORY_CACHING"] = "1"
+#os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+#os.environ["TORCH_USE_CUDA_DSA"] = "1"
+#os.environ["PYTORCH_CUDA_ALLOC_CONF"] =
 
 class TelegramDataset(Dataset):
-    def __init__(self, filepath, model, tokenizer, max_length=256):
+    def __init__(self, filepath, model, tokenizer, max_length=256, seq_len=10, overlap = 1):
         self.filepath = filepath
         self.model = model
-        self.seq_len = 10
+        self.seq_len = seq_len
         self.tokenizer = tokenizer
+        self.overlap = overlap
         self.max_length = max_length
         # Messages contain a sequence of sender-message pairs
-        self.messages = pd.read_csv(self.filepath,sep = ";", encoding="utf-8").iloc[0:100,:]
+        self.messages = pd.read_csv(self.filepath,sep = ";", encoding="utf-8")
         # Drop if the message has over 80 characters
-        self.messages = self.messages[self.messages['message'].str.len() < 80]
+        self.messages = self.messages[self.messages['message'].str.len() < 120]
         self.messages = self.messages.reset_index(drop=True)
         # Change 30% of the senders to "GPT"
         #self.messages['sender'] = self.messages['sender'].apply(lambda x: "GPT" if random.random() < 0.3 else x)
         print(f"Number of messages: {len(self.messages)}")
-        self.prompts = self.create_prompts()
+        self.prompts = self.create_prompts(overlap=self.overlap)
         random.shuffle(self.prompts)
         print(f"Number of prompts: {len(self.prompts)}")
 
-    def create_prompts(self):
-        """ Combine the messages into prompts and targets. """
+    def create_prompts(self, overlap = 1):
+        """ Combine the messages into prompts, where the prompt is the previous seq_len messages, with the last message sender changed to "GPT".
+        """
+        # Create a list of prompts, where each prompt is a sequence of messages
         prompts = []
-        targets = []
-        for i in range(len(self.messages) - self.seq_len):
+        # Maintain a list of the last seq_len-1 messages
+        last_seq_len_messages = self.messages.iloc[0:self.seq_len-1,:].reset_index(drop=True)
+        # Loop through messages starting from the seq_len'th message,
+        # since the first seq_len-1 messages are in the last_seq_len_messages list
+        # Create a prompt, and update the last_seq_len_messages list
+        for i in range(self.seq_len, len(self.messages)):
             prompt = ""
-            for j in range(self.seq_len):
-                prompt += f"{self.messages['sender'][i+j]}:{self.messages['message'][i+j]}\n"
+            for j in range(self.seq_len-1):
+                prompt += f"{last_seq_len_messages['sender'][j]}:{last_seq_len_messages['message'][j]}\n"
             prompt += "GPT:"
-            #target_sender = self.messages['sender'][i+self.seq_len]
-            # Change the target senders name to "GPT" in the prompt
-            #prompt = prompt.replace(target_sender, "GPT")
-            #target = prompt + self.messages['message'][i+self.seq_len]
-            prompt = prompt + self.messages['message'][i+self.seq_len]
+            prompt = prompt + self.messages['message'][i]
             prompts.append(prompt)
-            #targets.append(target)
-        return prompts#, targets
-
+            # Update the last seq_len-1 messages
+            last_seq_len_messages = self.messages.iloc[i-self.seq_len+1:i,:].reset_index(drop=True)
+        overlap = self.seq_len if overlap is None else overlap
+        # Only pick every overlap'th prompt
+        prompts = prompts[::overlap]
+        return prompts
+            
     def __len__(self):
         return len(self.prompts) - self.seq_len
     
@@ -82,7 +93,11 @@ def test_model(model, tokenizer, ds):
         print(f"No 'GPT:' in prompt: {prompt}")
         return
     prompt = prompt_split[0] + "GPT:"
-    target = prompt_split[1]
+    try:
+        target = prompt_split[1]
+    except IndexError:
+        print(f"No target in prompt: {prompt}")
+        return
     # Get the predicted next message
     prompt_tensor = tokenizer(prompt, return_tensors='pt')['input_ids']
     output = model.generate(prompt_tensor, do_sample=True, temperature=0.6, max_new_tokens=50, no_repeat_ngram_size=2)
@@ -104,34 +119,42 @@ def load_model(model_name):
     
 if __name__ == "__main__":
     # Load the dataset
-    model_name = 'TurkuNLP/gpt3-finnish-large'
-    model_name = "gpt3-finetuned"
+    #model_name = 'TurkuNLP/gpt3-finnish-small'
+    #model_name = "gpt3-finetuned-telegram"
+    model_name = "gpt3-xl-finetuned"
     model, tokenizer = load_model(model_name)
-    dataset = TelegramDataset("_chat_history.csv", model, tokenizer)
-    dataset_test = TelegramDataset("_chat_history_test.csv", model, tokenizer)
-
-
+    dataset = TelegramDataset("_chat_history.csv", model, tokenizer, max_length=256, seq_len=10, overlap=9)
+    dataset_test = TelegramDataset("_chat_history_test.csv", model, tokenizer, max_length=256, seq_len=10, overlap=9)
+    
+    # Clean up the cache
+    torch.cuda.empty_cache()
+    # Reserve all the GPU memory
+    #torch.cuda.set_per_process_memory_fraction(0.99, None)
+    
     for i in range(4):
+        pass
         test_model(model, tokenizer, dataset_test)
+    exit()
     # Train the model
     training_args = TrainingArguments(
         output_dir='./results',
-        num_train_epochs=1,
+        num_train_epochs=10,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
         warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
         logging_steps=10,
-        save_steps=200,
-        save_total_limit=2,
+        #save_steps=200,
+        #save_total_limit=2,
+        #fp16=True,
+        #fp16_opt_level='O0',
+        
     )
     trainer = Trainer(
         model=model,
         args=training_args,
         train_dataset=dataset,
-        #eval_dataset=dataset,
-        #data_collator=data_collator,
     )
     trainer.train()
     trainer.save_model("./gpt3-finetuned-telegram")
