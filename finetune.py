6b93de0aaff58468f3b3345b7d7849d15d0904b8
ilmari99 <i.vahteristo@gmail.com>
2023-07-19 10:34:15 +0300

    New finetuning. Maybe a bit cleaner

--- finetune.py
+++ finetune.py
@@ -1,186 +1,230 @@
 """
-This dataset reads a csv file of sequential messages from a Telegram chat.
-The data contains each messages sender and the message itself.
+This dataset reads a csv file of sequential messages from a Telegram chat, in the format:
+id;time;from;text;reply_to_message_id
+```
+1189;21:12;Pauli lut;Saisko tldr kokouksest😂;
+1190;21:13;Taru Haimi;Tervetuloa hallitukseen 😎;
+1191;21:13;Joona;Tää 😄;1190
+1192;21:13;Taru Haimi;(image or file);
+1193;21:13;Taru Haimi;Siinä kokoonpano;1192
+1194;22:49;Akseli Suutari;"Onnittelut viel @tartza 
+Piti lähtä treeneihi kesken kokouksen";
+1195;23:18;Taru Haimi;Kiitoskiitos 😎;
+```
 
-This file contains a convenient class, TelegramDataset, for reading the data and using the data to fine tune a language model.
+This data is used to finetune a GPT-3 model, and the resulting model is used to generate new messages based on previous messages.
 
-The goal is to use this dataset to finetune a GPT-3 model, to generate text that resembles the chat history.
-Chat history is represented like: "sender: message\nsender: message\n...".
-
-The model is finetuned to predict the next message, given the previous n messages, by making the prompt: "sender: message\nsender: message\n...GPT:"
+The model is tuned by giving it segments of the chat history (first id starting from random -10000-10000).
+In a segment:
+- no sender is changed to "GPT"
+- The sender of the last message is changed to "GPT"
+- All occurrences of the last sender in the segment are changed to "GPT"
 """
-import json
-import time
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.cuda.amp import autocast
-from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
+from transformers import TextDataset, DataCollatorForLanguageModeling, AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 import random
 import numpy as np
 import os
-#os.environ["PYTORCH_NO_CUDA_MEMORY_CACHING"] = "1"
-#os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-#os.environ["TORCH_USE_CUDA_DSA"] = "1"
-#os.environ["PYTORCH_CUDA_ALLOC_CONF"] =
+from LanguageModel import LanguageModel
 
-class TelegramDataset(Dataset):
-    def __init__(self, filepath, model, tokenizer, max_length=256, seq_len=10, overlap = 1):
-        self.filepath = filepath
-        self.model = model
-        self.seq_len = seq_len
-        self.tokenizer = tokenizer
-        self.overlap = overlap
-        self.max_length = max_length
-        # Messages contain a sequence of sender-message pairs
-        self.messages = pd.read_csv(self.filepath,sep = ";", encoding="utf-8")[0:300]
-        # Drop if the message has over 80 characters
-        self.messages = self.messages[self.messages['message'].str.len() < 200]
-        self.messages = self.messages.reset_index(drop=True)
-        # Change 30% of the senders to "GPT"
-        #self.messages['sender'] = self.messages['sender'].apply(lambda x: "GPT" if random.random() < 0.3 else x)
-        print(f"Number of messages: {len(self.messages)}")
-        self.prompts = self.create_prompts(overlap=self.overlap)
-        random.shuffle(self.prompts)
-        print(f"Number of prompts: {len(self.prompts)}")
+def load_dataset(path, test_split_frac = 0.1):
+    """ Load a dataset from a csv file.
+    """
+    # Load the data
+    messages = pd.read_csv(path,sep = ";", encoding="utf-8")
+    # Drop if the message has over max_msg_len characters
+    messages = messages.reset_index(drop=True)
+    # Split the data into train and test sets
+    train_size = int(len(messages) * (1-test_split_frac))
+    if test_split_frac == 0:
+        test_dataset = pd.DataFrame()
+    else:
+        test_size = len(messages) - train_size
+        train_dataset, test_dataset = train_test_split(messages, test_size=test_size, shuffle = False)
+    return train_dataset, test_dataset
 
-    def create_prompts(self, overlap = 1):
-        """ Combine the messages into prompts, where the prompt is the previous seq_len messages, with the last message sender changed to "GPT".
-        """
-        # Create a list of prompts, where each prompt is a sequence of messages
-        prompts = []
-        # Maintain a list of the last seq_len-1 messages
-        last_seq_len_messages = self.messages.iloc[0:self.seq_len,:].reset_index(drop=True)
-        # Loop through messages starting from the seq_len'th message,
-        # since the first seq_len-1 messages are in the last_seq_len_messages list
-        # Create a prompt, and update the last_seq_len_messages list
-        for i in range(self.seq_len, len(self.messages)):
-            prompt = ""
-            # Get all messages except the last one
-            for j in range(self.seq_len):
-                prompt += f"{last_seq_len_messages['sender'][j]}({last_seq_len_messages['time'][j]}):{last_seq_len_messages['message'][j]}\n"
-            # Change j to be te index of the last message in last_seq_len_messages window
-            #j += 1
-            #print(f"prompt: {prompt}")
-            actual_sender = last_seq_len_messages['sender'][j]
-            #actual_message = last_seq_len_messages['message'][j]
-            #print(f"Actual sender: {actual_sender}")
-            #prompt += actual_sender + ":"+last_seq_len_messages['message'][self.seq_len-1]
-            actual_sender = last_seq_len_messages['sender'][j]
-            #print(f"Actual sender: {actual_sender}")
-            prompt = prompt.replace(f"{actual_sender}", "GPT")
-            #print(f"NEW prompt: {prompt}")
-            prompts.append(prompt)
-            # Update the last_seq_len_messages list by removing the first message, and appending the next
-            last_seq_len_messages = last_seq_len_messages.iloc[1:,:].reset_index(drop=True)
-            last_seq_len_messages = pd.concat([last_seq_len_messages, self.messages.iloc[i,:].to_frame().transpose()], axis=0).reset_index(drop=True)
-        overlap = self.seq_len if overlap is None else overlap
-        # Only pick every overlap'th prompt
-        prompts = prompts[::overlap]
-        return prompts
-
-    def __len__(self):
-        return len(self.prompts) - self.seq_len
-
-    def __getitem__(self, idx):
-        prompt = self.prompts[idx]
-        # Encode the prompt, and pad/truncate it to a fixed length
-        # The truncating is done from the left, so the last message is always included
-        input = self.tokenizer(prompt, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
-
-        return {"input_ids": input['input_ids'].squeeze(), "labels": input['input_ids'].squeeze()}
-
-def test_model(model, tokenizer, ds):
-    """ Test the model on the dataset. """
-    # Get a random sample
-    idx = random.randint(0, len(ds)-1)
-    sample = ds[idx]
-    # Get the prompt
-    prompt = tokenizer.decode(sample['input_ids'], skip_special_tokens=True)
-    try:
-        # Get the target, which is the last message after "GPT:"
-        # Find the last "GPT:" in the prompt
-        prompt_split = prompt.rsplit("GPT", 1)
-        #print(prompt_split)
-    except:
-        print(f"No 'GPT:' in prompt: {prompt}")
-        return
-    prompt = prompt_split[0] + "GPT:"
-    try:
-        target = prompt_split[1]
-    except IndexError:
-        print(f"No target in prompt: {prompt}")
-        return
-    # Get the predicted next message
-    pre_prompt = "GPT on hauska ja vitsikäs Teekkari-botti;\n"
-    prompt = pre_prompt + prompt
-    prompt_tensor = tokenizer(prompt, return_tensors='pt')['input_ids']
-    output = model.generate(prompt_tensor, do_sample=True, temperature=0.6, max_new_tokens=50, no_repeat_ngram_size=2)
-    output = tokenizer.decode(output.squeeze(), skip_special_tokens=True)
-    output = output[len(prompt):].split('\n')[0]
-    # Print the results
-    print(f"Prompt: {prompt}")
-    print(f"Target: {target}")
-    print(f"Output: {output}")
-    print()
-
-
-def load_model(model_name):
-    """ Load model and tokenizer from the results directory. """
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+def load_model(model_name = "TurkuNLP/gpt3-finnish-small"):
+    """ Load a pretrained GPT model and tokenizer
+    """
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
     return model, tokenizer
 
+class ChatHistoryDataset(Dataset):
+    """ Takes in a dataframe of sequential messages from a Telegram chat,
+    and batches them into segments, which are used to finetune a GPT-3 model.
+    Each message has the following: id;time;from;text;reply_to_message_id
+    """
+    def __init__(self, df, tokenizer, max_msg_len = 200, segment_len = 15, max_segments = None):
+        self.df = df
+        self.tokenizer = tokenizer
+        self.max_msg_len = max_msg_len
+        self.segment_len = segment_len
+        self.max_segments = max_segments
+        
+        # Drop if the message has over max_msg_len characters
+        self.parsed_df = self.df[self.df["text"].str.len() < self.max_msg_len]
+        print(f"Number of messages after dropping messages with over {self.max_msg_len} characters: {len(self.parsed_df)}")
+        
+        self.segments = self._create_segments()
+        
+        
+        
+    def _create_segments(self):
+        """ Create len(df) / segment_len dataframes where each dataframe has segment_len messages.
+        In each dataframe, all the occurrences of the last sender are changed to "GPT", and 
+        """
+        segments = []
+        for i in range(0, len(self.parsed_df), self.segment_len):
+            segment_df = self.parsed_df.iloc[i:i+self.segment_len]
+            segment_df = self._prepare_segment(segment_df)
+            segments.append(segment_df)
+        return segments
     
+    def segment_as_prompt(self, segment_df):
+        """ Convert a segment dataframe to a string, that looks like:
+        ```
+        1189;21:12;GPT;Saisko tldr kokouksest😂;
+        1190;21:13;Taru Haimi;Tervetuloa hallitukseen 😎;
+        1191;21:13;Joona;Tää 😄;1190
+        1192;21:13;Taru Haimi;(image or file);
+        1193;21:13;Taru Haimi;Siinä kokoonpano;1192
+        1194;22:49;Akseli Suutari;"Onnittelut viel @tartza 
+        Piti lähtä treeneihi kesken kokouksen";
+        1195;23:18;Taru Haimi;Kiitoskiitos 😎;
+        1196;23:30;GPT;Nice, kiva homma 😂;
+        ```
+        """
+        return "\n".join(segment_df[["id", "time", "from", "text", "reply_to_message_id"]].astype(str).agg(";".join, axis=1))
+    
+    def _prepare_segment(self, segment_df):
+        """ Prepare a segment of messages by changing the sender of the last message to "GPT" and all the occurrences of the last sender to "GPT".
+        Also change the indexes.
+        """
+        segment_df = segment_df.copy()
+        last_sender = segment_df.iloc[-1]["from"]
+        # Change all the occurrences of the last sender to "GPT"
+        segment_df["from"] = segment_df["from"].replace(last_sender, "GPT")
+        
+        # Change id to start from a random number -1000 - 1000 and change all 'reply_to_message_id' to match the new ids
+        start_random_id = random.randint(-1000,1000)
+        old_ids = segment_df["id"].values
+        new_ids = list(range(start_random_id, start_random_id + len(segment_df)))
+        id_map = {old_id: new_id for old_id, new_id in zip(old_ids, new_ids)}
+        id_map[pd.NA] = pd.NA
+        # Change the 'id' column
+        segment_df["id"] = segment_df["id"].apply(lambda x: id_map[x])
+        
+        # Change the 'reply_to_message_id' column
+        segment_df["reply_to_message_id"] = segment_df["reply_to_message_id"].apply(lambda x: id_map.get(x, ""))
+        
+        return segment_df
+    
+    def __len__(self):
+        return len(self.segments)
+    
+    def __getitem__(self, idx):
+        text = self.segment_as_prompt(self.segments[idx])
+        encoding = self.tokenizer(text, return_tensors="pt", truncation=True, max_length=self.max_msg_len, padding="max_length")
+        input_ids = encoding["input_ids"].squeeze()
+        attention_mask = encoding["attention_mask"].squeeze()
+        return {"input_ids": input_ids, "attention_mask": attention_mask}
+
+def test_on_prompts(model, tokenizer, dataset, num_prompts = 10, pre_prompt = ""):
+    """ Test the model on random prompts from the dataset.
+    Print the prompt, model's response, and the actual response (last message in the segment).
+    """
+    lang_model = LanguageModel(model_name="GPT", model=model, tokenizer=tokenizer)
+    for i in range(num_prompts):
+        idx = random.randint(0, len(dataset)-1)
+        # Get the segment_df
+        segment_df = dataset.segments[idx]
+        # Remove and store the last message
+        last_message = segment_df.iloc[-1]
+        segment_df.drop(segment_df.tail(1).index, inplace=True)
+        
+        middle_prompt = dataset.segment_as_prompt(segment_df)
+        
+        # Add the last message to the prompt up to the username
+        prompt = pre_prompt + middle_prompt + "\n" + str(last_message["id"]) + ";" + last_message["time"] + ";" + str(last_message["from"]) + ";"
+        
+        print(f"Prompt {idx}\n----------------------------------- \n", prompt, "\n-----------------------------------")
+        pred_resp = lang_model.get_only_new_tokens(prompt, temperature=0.6, max_new_tokens=100)
+        true_resp = last_message["text"] + ";" + str(last_message["reply_to_message_id"])
+        print(f"Predicted response: {pred_resp}")
+        print(f"True response: {true_resp}")
+        
+        
+    
+
 if __name__ == "__main__":
-    # Load the dataset
-    #model_name = 'TurkuNLP/gpt3-finnish-small'
-    #model_name = "gpt3-finetuned-telegram"
-    model_name = "gpt3-xl-finetuned-time"
-    model, tokenizer = load_model(model_name)
-    #dataset = TelegramDataset("_chat_history.csv", model, tokenizer, max_length=256, seq_len=15, overlap=9)
-    dataset_test = TelegramDataset("_chat_history_times.csv", model, tokenizer, max_length=256, seq_len=15, overlap=9)
+    #MODEL_NAME = "TurkuNLP/gpt3-finnish-small"
+    MODEL_NAME = "gpt3-xl-finetuned-v3"
+    SAVE_PATH = "./finetuned_model"
+    TEST_RUN = False
+    RUN_EVALS = [True, False]
     
-    # Clean up the cache
-    torch.cuda.empty_cache()
-    # Reserve all the GPU memory
-    #torch.cuda.set_per_process_memory_fraction(0.99, None)
+    train_data_df, test_data_df = load_dataset("_chat_history.csv")
+    if TEST_RUN:
+        train_data_df = train_data_df[:100]
+        test_data_df = test_data_df[:100]
     
-    for i in range(6):
-        pass
-        test_model(model, tokenizer, dataset_test)
+    model, tokenizer = load_model(MODEL_NAME)
+    
+    train_ds = ChatHistoryDataset(train_data_df, tokenizer)
+    test_ds = ChatHistoryDataset(test_data_df, tokenizer)
+    
+    if RUN_EVALS[0]:
+        # Test model on random prompts
+        pre_prompt = "Olet GPT teekkari tekoäly, joka keksii hauskoja vastauksia viesteihin ryhmäkeskustelussa. Sinut on koulutettu keskustelun historialla ja olet hauska ja ystävällinen tekoäly joka viihdyttää ihmisiä."
+        pre_prompt += "Vastauksesi tulee jatkaa keskustelua samalla formaatilla, eli \"<id>;<time>;<sender>;<message>;<reply_to_id>\". Tässä viimeisimmät viestit:\n"
+        test_on_prompts(model, tokenizer, test_ds, num_prompts=5, pre_prompt=pre_prompt)
     exit()
-    # Train the model
+    
+    print(f"Number of segments in train: {len(train_ds)}")
+    print(f"Number of segments in test: {len(test_ds)}")
+    
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    #exit()
     training_args = TrainingArguments(
-        output_dir='./results',
-        num_train_epochs=10,
+        output_dir="./finetuned_model",
+        num_train_epochs=3,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
         warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
         logging_steps=10,
-        #save_steps=200,
-        #save_total_limit=2,
-        #fp16=True,
-        #fp16_opt_level='O0',
-        
     )
+    
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=dataset,
+        train_dataset=train_ds,
+        eval_dataset=test_ds,
+        data_collator=data_collator,
     )
-    trainer.train()
-    trainer.save_model("./gpt3-finetuned-telegram")
-    # Save the tokenizer
-    tokenizer.save_pretrained("./gpt3-finetuned-telegram")
-    # Save the dataset
-    torch.save(dataset, "./gpt3-finetuned-telegram/dataset.pt")
-    print("Models saved.")
-    for i in range(4):
-        test_model(model, tokenizer, dataset_test)
-
     
-
-
+    trainer.train()
+    # Save model and tokenizer
+    trainer.save_model(SAVE_PATH)
+    tokenizer.save_pretrained(SAVE_PATH)
+    
+    if RUN_EVALS[1]:
+        # Move everything to the same device before inference
+        model.to("cuda")
+        # Test model on random prompts
+        test_on_prompts(model, tokenizer, test_ds, num_prompts=5, pre_prompt="")
+    
+    
+    
+    
+    
+    
+    
+    
