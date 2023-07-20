36dc44b3ac92ac5e0b9c5f996c3039e37e21f9e8
ilmari99 <i.vahteristo@gmail.com>
2023-07-20 11:09:42 +0300

    Added 'get_only_until_token'

--- LanguageModel.py
+++ LanguageModel.py
@@ -17,33 +17,8 @@ class LanguageModel:
         else:
             self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    def create_profile_for_user(self, username, messages, prev_knowledge = None):
-        """
-        Create a profile for a user based on a sequence of messages.
-
-        Parameters
-        username: str
-            The username, for who to create the profile for.
-        messages: str
-            The messages to create the profile from.
-        prev_knowledge: str
-            The previous knowledge to use when creating the profile. I.e. the previous profile.
-        """
-        if prev_knowledge:
-            curr_profile = prev_knowledge
-        else:
-            curr_profile = f"{username} on opiskelija Lappeenrannan teknillisessä yliopistossa."
-        # Create the prompt
-        prompt = f"Tehtävänäsi on kirjoittaa n. 50 sanan analyysi {username}:sta taustatiedon, sekä lyhyen viesti keskustelun perusteella hyödyntäen persoonallisuus psykologiaa sekä stereotypioita.\n\n"
-        prompt += f"Taustatieto: \"{curr_profile}\"\n\n"
-        prompt += "Lyhyt viesti ketju:\n\n\"" + messages + "\""
-        prompt += f"\n\nPersoonallisuus analyysi {username}:sta:"
-        print(f"Prompt:\n{prompt}\n")
-        # Generate the new profile
-        new_profile = self.get_only_new_tokens(prompt, temperature=0.8, max_new_tokens=100)
-        return new_profile
-
-    def generate(self, prompt, temperature=0.7, max_new_tokens=50):
+    def generate(self, prompt, temperature=0.5, max_new_tokens=50):
+        print(f"PROMPT --------------------------------------------\n{prompt}\n--------------------------------------------")
         input = self.tokenizer(prompt, return_tensors='pt')
         output = self.model.generate(
             **input,
@@ -52,15 +27,22 @@ class LanguageModel:
             max_new_tokens=max_new_tokens,
             no_repeat_ngram_size=3,
         )
-        return self.tokenizer.decode(output[0], skip_special_tokens=True)
+        out = self.tokenizer.decode(output[0], skip_special_tokens=False)
+        return out
     
-    def get_only_new_tokens(self, prompt, temperature=0.7, max_new_tokens=50):
+    def get_only_new_tokens(self, prompt, temperature=0.5, max_new_tokens=50):
         output = self.generate(prompt, temperature, max_new_tokens)
-        return output[len(prompt):]
+        out = output[len(prompt):]
+        print(f"Generated --------------------------------------------\n{out}\n--------------------------------------------")
+        return out
     
-    def get_only_next_line(self, prompt, temperature=0.7, max_new_tokens=50):
+    def get_only_next_line(self, prompt, temperature=0.5, max_new_tokens=50):
         output = self.get_only_new_tokens(prompt, temperature, max_new_tokens)
         return output.split('\n')[0]
+    
+    def get_only_until_token(self, prompt, temperature=0.5, max_new_tokens=50, token='[MEG]'):
+        output = self.get_only_new_tokens(prompt, temperature, max_new_tokens)
+        return output.split(token)[0] + token
 
 
 
