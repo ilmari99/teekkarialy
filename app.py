1d1f2a7f098a3fe7b2d0e8270d607b9b77b3420e
ilmari99 <i.vahteristo@gmail.com>
2023-07-20 11:06:58 +0300

    Changed to use [FS] [MSG] [MEG] as special tokens. Added /gpt-info and /vitsi commands

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 import random
 from BotHead import BotHead
-from MessageActions import TriggerPhrase
+from MessageActions import LMGenerateOnTriggerPhrase, MakeJoke, GiveCommandInformation, OnFirstMessageInNewChat
 
 with open("_token.txt", "r") as f:
     TOKEN = f.read().strip()
 
-MODEL_NAME = 'gpt3-xl-finetuned-v3'
-N_MESSAGES = 10
+#MODEL_NAME = 'gpt3-xl-finetuned-v3-2-2Epoch'
+MODEL_NAME = "gpt3-xl-finetuned-v4-sq-br"
+N_MESSAGES = 16
 CHAT_TYPES = ['group', 'supergroup', 'private', 'channel', 'bot' ]
 bot = BotHead(MODEL_NAME, TOKEN, N_MESSAGES)
 
-MESSAGE_ACTIONS = [TriggerPhrase]
+MESSAGE_ACTIONS = [
+    OnFirstMessageInNewChat,
+    GiveCommandInformation,
+    MakeJoke,
+    LMGenerateOnTriggerPhrase,
+]
 MESSAGE_ACTIONS = [action(bot) for action in MESSAGE_ACTIONS]
 
-# One function, which handles all incoming messages.
-@bot.tg_bot.message_handler(func=lambda message: True)
+# One function, which handles all incoming messages. Also messaages from other bots
+@bot.tg_bot.message_handler(func=lambda message: True,)
 def message_stack_handler(message):
     bot.store_item(message)
     for action in MESSAGE_ACTIONS:
         triggered = action(message)
+        if triggered not in [True, False]:
+            print(f"Action {action} returned {triggered}. Actions must return True or False.")
+            break
         if triggered:
+            print(f"Action '{action.__class__.__name__}' triggered.")
             break
 
 bot.run()
