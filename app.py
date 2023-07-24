0006e44fe42e50ee135502b5c14497cf7d87fdba
ilmari99 <i.vahteristo@gmail.com>
2023-07-24 23:18:35 +0300

    Added accepting for most common content_types

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ with open("_token.txt", "r") as f:
     TOKEN = f.read().strip()
 
 #MODEL_NAME = 'gpt3-xl-finetuned-v3-2-2Epoch'
-MODEL_NAME = "gpt3-xl-finetuned-v4-sq-br"
+MODEL_NAME = "gpt3-xl-finetuned-bigdata-v1"
 N_MESSAGES = 16
 CHAT_TYPES = ['group', 'supergroup', 'private', 'channel', 'bot' ]
 bot = BotHead(MODEL_NAME, TOKEN, N_MESSAGES)
@@ -20,7 +20,7 @@ MESSAGE_ACTIONS = [
 MESSAGE_ACTIONS = [action(bot) for action in MESSAGE_ACTIONS]
 
 # One function, which handles all incoming messages. Also messaages from other bots
-@bot.tg_bot.message_handler(func=lambda message: True,)
+@bot.tg_bot.message_handler(func=lambda message: True, content_types=["text","sticker","photo","audio","video","document"])
 def message_stack_handler(message):
     bot.store_item(message)
     for action in MESSAGE_ACTIONS:
